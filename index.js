accbff513b2958e2aafb59fee6f11b1a6b3c6c5f
James <jamesschmidt124@gmail.com>
2021-09-20 19:36:42 -0700

    commit tree f3a683837f19ba2d42bb33cb19ffc2cdfab1023b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+let random = 2
+Number.parseInt(Math.random(random));
+let num3 = 29;
+let num4 = 5
+let mod = num3 %= num4;
+let max = (1, 3, 5, 20)
+Math.max(max);
 
-// Write your code here
